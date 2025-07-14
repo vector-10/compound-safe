0bd4bc67768350634434d8cf2a873654120239f5
vector-10 <duzieblaise10@gmail.com>
2025-07-14 04:38:41 +0100

    chore: wallet connect

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Web3Provider from "@/components/Web3Provider";
+import Web3Provider from "../components/Web3Provider";
 import "./globals.css";
 
 const geistSans = Geist({

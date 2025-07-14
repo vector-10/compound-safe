ce65bdc6353f66d9239473add7a755973901652a
vector-10 <duzieblaise10@gmail.com>
2025-07-14 04:42:42 +0100

    chore: updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Web3Provider from "../components/Web3Provider";
+import Web3Provider from "@/components/Web3Provider";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
+  
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],

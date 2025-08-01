1867c3c0f40ea86adac266aa263a1176fad73048
vector-10 <duzieblaise10@gmail.com>
2025-08-01 16:44:17 +0100

    chore: updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
 "use client"
 import React from 'react';
 import Link from 'next/link';
-import { Github, Twitter, Instagram, Shield } from 'lucide-react';
+import { Shield } from 'lucide-react';
+import { FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaDiscord } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -9,32 +11,30 @@ const Footer = () => {
       <div className="absolute inset-0 bg-gradient-to-b from-gray-900/0 via-gray-00/90 to-black z-0"></div>
       
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
-        <div className="flex flex-col md:flex-row justify-between">
+        <div className="flex flex-col md:flex-row justify-around ">
           <div className="mb-10 md:mb-0 md:w-1/2 lg:w-2/5 pr-8">
             
           <div className="flex items-center space-x-1 lg:space-x-2">
-                        <span className="text-lg lg:text-xl font-semibold text-gray-900 dark:text-white">
+                        <span className="text-lg lg:text-2xl font-semibold text-gray-900 dark:text-white">
                             CompoundSafe
                         </span>
                         <Shield className="w-5 h-5 lg:w-6 lg:h-6 text-blue-600" />
                     </div>
             
-            <p className="text-sm leading-relaxed mb-8 text-gray-400">
+            <p className="text-sm lg:text-md leading-relaxed mb-8 text-gray-400">
               We're dedicated to reshaping the landscape of liquidity management, maximize yield opportunities, and participate in the thriving DeFi ecosystem.
             </p>
             
             <div className="flex space-x-4">
+            <a href="#" className="text-gray-500 hover:text-white transition-colors">
+                <FaDiscord size={20} />
+              </a>
+             
               <a href="#" className="text-gray-500 hover:text-white transition-colors">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                  <path d="M12 5.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM19 12a7 7 0 11-14 0 7 7 0 0114 0z" />
-                  <path d="M9 12a3 3 0 106 0 3 3 0 00-6 0z" fill="currentColor" />
-                </svg>
+                <FaTwitter size={20} />
               </a>
               <a href="#" className="text-gray-500 hover:text-white transition-colors">
-                <Twitter size={20} />
-              </a>
-              <a href="#" className="text-gray-500 hover:text-white transition-colors">
-                <Instagram size={20} />
+              <FaInstagram size={20} />
               </a>
             </div>
           </div>
@@ -101,7 +101,7 @@ const Footer = () => {
         
         {/* Copyright */}
         <div className="mt-12 pt-8 border-t border-gray-800/30">
-          <p className="text-gray-500 text-xs text-center">
+          <p className="text-gray-500 text-sm text-center">
             © {new Date().getFullYear()} CompoundSafe. All rights reserved.
           </p>
         </div>
